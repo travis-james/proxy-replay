c78cecf21aa64c3dc63abf05fb3c459e0057e556
Travis Henderson <tjh@Traviss-MacBook-Air.local>
2026-03-11 14:56:06 +0900

    fileStorage done, next integrate with recorder?

--- internal/storage/fileStorage.go
+++ internal/storage/fileStorage.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 type FileStorage struct {
@@ -54,6 +55,7 @@ func (fs FileStorage) Save(key string, rec Recording) (err error) {
 	return os.Rename(tmpFile.Name(), finalPath)
 }
 
+// Load a saved req/resp.
 func (fs FileStorage) Load(key string) (rec Recording, err error) {
 	finalPath := filepath.Join(fs.Dir, key+".json")
 	data, err := os.ReadFile(finalPath)
@@ -69,5 +71,33 @@ func (fs FileStorage) Load(key string) (rec Recording, err error) {
 }
 
 func (fs FileStorage) List() (metaData []RecordingMeta, err error) {
-	return
+	entries, err := os.ReadDir(fs.Dir)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, entry := range entries {
+		if entry.IsDir() {
+			// skip directories.
+			continue
+		}
+		if filepath.Ext(entry.Name()) != ".json" {
+			// skip anything that is not .json
+			continue
+		}
+
+		info, err := entry.Info()
+		if err != nil {
+			return nil, err
+		}
+
+		fileName := strings.TrimSuffix(entry.Name(), ".json")
+
+		metaData = append(metaData, RecordingMeta{
+			Key:       fileName,
+			Timestamp: info.ModTime(),
+			SizeBytes: info.Size(),
+		})
+	}
+	return metaData, nil
 }
--- internal/storage/fileStorage_test.go
+++ internal/storage/fileStorage_test.go
@@ -1,51 +1,94 @@
 package storage
 
 import (
+	"encoding/json"
+	"os"
+	"path/filepath"
 	"reflect"
 	"testing"
-
-	"github.com/travis-james/proxy-replay/internal/recorder"
 )
 
-func TestFileStorage_SaveAndLoad(t *testing.T) {
-	// set up.
-	var (
-		dir      = t.TempDir()
-		fs       = FileStorage{Dir: dir}
-		fileName = "test-recording"
-		req      = recorder.RecordedRequest{
-			Method: "GET",
-			URL:    "https://example.com",
-			Headers: map[string][]string{
-				"Accept": {"application/json"},
-			},
-			Body: []byte("hello"),
-		}
-		resp = recorder.RecordedResponse{
-			StatusCode: 200,
-			BodyBase64: "aGVsbG8=",
-			Headers: map[string][]string{
-				"Content-Type": {"application/json"},
-			},
-		}
-		rec = Recording{
-			Request:  req,
-			Response: resp,
-		}
-	)
+func TestFileStorage_Save(t *testing.T) {
+	dir := t.TempDir()
+	fs := FileStorage{Dir: dir}
 
-	// run.
-	err := fs.Save(fileName, rec)
+	rec := Recording{}
+
+	err := fs.Save("test", rec)
 	if err != nil {
-		t.Fatalf("Save failed: %v", err)
+		t.Fatalf("save failed: %v", err)
 	}
 
-	loaded, err := fs.Load(fileName)
+	path := filepath.Join(dir, "test.json")
+	if _, err := os.Stat(path); err != nil {
+		t.Fatalf("expected file to exist: %v", err)
+	}
+}
+
+func TestFileStorage_Load(t *testing.T) {
+	dir := t.TempDir()
+	fs := FileStorage{Dir: dir}
+	key := "test"
+	finalPath := filepath.Join(fs.Dir, key+".json")
+
+	rec := Recording{}
+
+	data, err := json.MarshalIndent(rec, "", " ")
 	if err != nil {
-		t.Fatalf("Load failed: %v", err)
+		t.Fatalf("marshal failed: %v", err)
 	}
 
-	if !reflect.DeepEqual(rec, loaded) {
-		t.Fatalf("recording mismatch\nexpected: %+v\ngot: %+v", rec, loaded)
+	err = os.WriteFile(finalPath, data, 0644)
+	if err != nil {
+		t.Fatalf("write failed: %v", err)
+	}
+
+	got, err := fs.Load(key)
+	if err != nil {
+		t.Fatalf("load faield: %v", err)
+	}
+
+	if !reflect.DeepEqual(got, rec) {
+		t.Fatalf("expected:\n%v\ngot:%v\n", rec, got)
+	}
+}
+
+func TestFileStorage_List(t *testing.T) {
+	dir := t.TempDir()
+	fs := FileStorage{Dir: dir}
+	key := "test"
+	finalPath := filepath.Join(fs.Dir, key+".json")
+
+	rec := Recording{}
+
+	data, err := json.MarshalIndent(rec, "", " ")
+	if err != nil {
+		t.Fatalf("marshal failed: %v", err)
+	}
+
+	err = os.WriteFile(finalPath, data, 0644)
+	if err != nil {
+		t.Fatalf("write failed: %v", err)
+	}
+
+	got, err := fs.List()
+	if err != nil {
+		t.Fatalf("load faield: %v", err)
+	}
+
+	if len(got) != 1 {
+		t.Fatalf("expected 1 recording, got %d", len(got))
+	}
+
+	if got[0].Key != key {
+		t.Fatalf("expected key: %s, got: %s", key, got[0].Key)
+	}
+
+	if got[0].SizeBytes <= 0 {
+		t.Fatalf("expected positive file size")
+	}
+
+	if got[0].Timestamp.IsZero() {
+		t.Fatalf("expected timestamp to be set")
 	}
 }
